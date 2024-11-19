86ad752b3ffed4fea7964e8d1a81da6534053758
ItsAvs <avni.morepatil@gmail.com>
2024-11-19 11:44:41 -0800

    created engine class

---
+++ Engine.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Engine {
+
+    private JFrame frame;
+    private int length = 1024;
+    private int width = 768;
+    public Engine (){
+        initializeFrame();
+    }
+
+
+    // Initialize the main frame
+    private void initializeFrame() {
+        frame = new JFrame("Game");
+        frame.setSize(length, width);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // Center the window
+        frame.setLayout(new BorderLayout());
+    }
+
+    //display images for backgrounds
+
+    private void displayScreen(String file){
+        frame.setSize(length, width);  
+        ImageIcon image = new ImageIcon(file);
+        frame.add(new JLabel(image));
+        frame.pack();
+        frame.setVisible(true);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+
+    }
+
+    private boolean isActionDone(ActionListener e){
+        return e.getKeyCode() == KeyEvent.VK_ENTER;
+        
+    }
+}
