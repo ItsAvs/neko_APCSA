cd6e7f7b645fb502adbc2eb7947ca5b863564749
ItsAvs <avni.morepatil@gmail.com>
2024-11-20 11:36:52 -0800

    fix file path por favor

--- NekoGame.java
+++ NekoGame.java
@@ -6,7 +6,7 @@ public class NekoGame {
 
     public static void main(String arg[]){
         Engine game = new Engine();
-        game.displayScreen("C:\\Users\\800024958\\Desktop\\neko_APCSA\\imgs\\neko.png");
+        game.displayScreen("C:\\Users\\800024958\\Desktop\\neko_APCSA\\imgs\\neko.png"); //fix filepath
     }
 
 }
