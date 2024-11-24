9c9cee43b9597f265c7211c9b9e0555d6157f21e
LuckyBunnyLee <luckybunnylee@gmail.com>
2024-11-24 15:36:40 -0800

    hello

--- Nemo.java
+++ Nemo.java
@@ -1,19 +1,33 @@
-public class Nemo extends NekoCharacter {
+public class Nemo {
+
+    private String characterName;
+    private int maxHealth;
+    private int health;
+    private int damage;
+
 
     public Nemo(String characterName, int maxHealth) {
-        super(characterName, maxHealth);
-
+        this.characterName = characterName;
+        this.maxHealth = maxHealth;
     }
 
-    public void attack(Enemy opponent) {
-        System.out.println(opponent.getName() + " attacks " + opponent.getName() + " for " + opponent.getAttackPower() + " damage!");
-        opponent.takeDamage(opponent.getAttackPower());
+    public void lapseBlue(Enemy opponent) {
+        System.out.println(opponent.getName() + " uses Blue on " + opponent.getName() + " for " + damage + " damage!");
+        opponent.takeDamage(damage);
     }
 
-    public void specialMove(Enemy opponent) {
-        int specialDamage = opponent.getAttackPower() * 2;
-        System.out.println(opponent.getName() + " uses a special move on " + opponent.getName() + " for " + specialDamage + " damage!");
-        opponent.takeDamage(specialDamage);
+    public void reversalRed(Enemy opponent) {
+        System.out.println(opponent.getName() + " uses Red on " + opponent.getName() + " for " + damage * 2 + " damage!");
+        opponent.takeDamage(damage * 2);
+    }
+
+    public void hollowPurple(Enemy opponent) {
+        System.out.println("Take the amplified and the reversal, then smash together those two different expressions of infinity to create and push out imaginary mass. Imaginary Technique: Hollow Purple");
+        opponent.takeDamage(damage * 6);
+    }
+
+    public void reverseCursedTechnique() {
+        health += maxHealth / 5;
     }
 
 }
