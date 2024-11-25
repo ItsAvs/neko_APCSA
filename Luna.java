70cf5d78db85595092fa634acaa94ddfe1dc7744
LuckyBunnyLee <luckybunnylee@gmail.com>
2024-11-24 16:20:35 -0800

    malevolent shrine

---
+++ Luna.java
@@ -0,0 +1,8 @@
+public class Luna extends Enemy {
+    public Luna() {
+        super("Luna", 80);
+        addAbility("Hiss", 10, 20, 1, 0.1);
+        addAbility("Shadow Leap", 20, 30, 3, 0.25);
+        addAbility("Claw", 15, 25, 2, 0.2);
+    }
+}
