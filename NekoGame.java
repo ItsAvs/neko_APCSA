87767f795fa67a2bd158414175b7aca2bd70d6e9
ItsAvs <avni.morepatil@gmail.com>
2024-12-02 11:27:35 -0800

    music added

--- NekoGame.java
+++ NekoGame.java
@@ -14,11 +14,15 @@ public class NekoGame {
 
      public static void main(String[] args) {
         Engine game = new Engine();
-        game.playMusic("./audios/konekonoosanpo.wav", 120, true);
-        game.displayScreen("./imgs/neko.png");
+        game.playMusic("./audios/start_music.wav", 120, true);
+        game.displayScreen("./imgs/start_screen.png");
         game.startKeyCheckLoop(); //check if enter pressed
-        game.displayText("hiiiii");
         game.stopMusicWithFadeOut("./audios/konekonoosanpo.wav", 500);
+
+        //First cutscene: introduction to cafe
+        game.displayScreen("./imgs/screen1.png");
+        game.playMusic("./audios/cut_scenes.wav", 120, true);
+
         
         //display level 
 
